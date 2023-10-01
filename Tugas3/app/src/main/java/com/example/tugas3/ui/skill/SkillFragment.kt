9309e4a61dc6611461cd2a76d8c69ecabdfe9367
Ardaza <stellardrone08@gmail.com>
2023-10-01 17:06:38 +0700

    update

---
+++ Tugas3/app/src/main/java/com/example/tugas3/ui/skill/SkillFragment.kt
@@ -0,0 +1,167 @@
+package com.example.tugas3.ui.skill
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.tugas3.R
+import kotlin.collections.ArrayList
+import java.util.*
+import androidx.navigation.fragment.findNavController
+import androidx.appcompat.widget.SearchView
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+class SkillFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    private lateinit var adapter: MyAdapter
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var skillArrayList : ArrayList<Sekil>
+    private lateinit var searchView: SearchView
+    lateinit var imageid : Array<Int>
+    lateinit var heading : Array<String>
+    lateinit var skill : Array<String>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_skill, container, false)
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment SkillFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            SkillFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        datainitialize()
+        val layoutManager = LinearLayoutManager(context)
+        recyclerView = view.findViewById(R.id.recycler_view)
+        recyclerView.layoutManager = layoutManager
+        recyclerView.setHasFixedSize(true)
+        adapter = MyAdapter(skillArrayList)
+        recyclerView.adapter = adapter
+        searchView = view.findViewById(R.id.searchView)
+
+        adapter.onItemClick = {
+            navigateToDetail(it.heading)
+        }
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                filterList(newText)
+                return true
+            }
+
+        })
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        param1 = null
+    }
+
+    private fun navigateToDetail(extraName: String){
+        findNavController().navigate(SkillFragmentDirections.actionNavSkillToSkillDetail(extraName))
+    }
+
+    private fun filterList(query: String?) {
+
+        if (query != null) {
+            val filteredList = ArrayList<Sekil>()
+            for (i in skillArrayList) {
+                if (i.heading.lowercase(Locale.ROOT).contains(query)) {
+                    filteredList.add(i)
+                }
+            }
+
+            if (filteredList.isEmpty()) {
+                Toast.makeText(context, "No Data Found", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                adapter.setFilteredList(filteredList)
+            }
+        }
+    }
+
+
+    private fun datainitialize(){
+        skillArrayList = arrayListOf<Sekil>()
+
+        imageid = arrayOf(
+            R.drawable.ic_c,
+            R.drawable.ic_c,
+            R.drawable.ic_c,
+            R.drawable.ic_java,
+            R.drawable.ic_java,
+            R.drawable.ic_java,
+            R.drawable.ic_godot,
+            R.drawable.ic_godot,
+            R.drawable.ic_godot,
+            R.drawable.ic_godot
+        )
+
+        heading = arrayOf(
+            getString(R.string.head_1),
+            getString(R.string.head_1),
+            getString(R.string.head_1),
+            getString(R.string.head_2),
+            getString(R.string.head_2),
+            getString(R.string.head_2),
+            getString(R.string.head_3),
+            getString(R.string.head_3),
+            getString(R.string.head_3),
+            getString(R.string.head_3)
+
+        )
+        getUserData()
+    }
+    private fun getUserData(){
+        for (i in imageid.indices){
+            val skill = Sekil(imageid[i],heading[i])
+            skillArrayList.add(skill)
+        }
+    }
+}
